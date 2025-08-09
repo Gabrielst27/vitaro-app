9aadf50f72060f426563e52a5234596dc515c654
Gabriel Torres <gabrielst328@gmail.com>
2025-08-09 18:33:32 -0300

    refac: improved edit workout screen

--- lib/ui/screens/edit_workout_screen.dart
+++ lib/ui/screens/edit_workout_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vitaro_app/domain/models/workouts_model.dart';
-import 'package:vitaro_app/ui/widgets/edit_workout/edit_workout_header.dart';
+import 'package:vitaro_app/ui/widgets/edit_workout/edit_workout_form.dart';
 import 'package:vitaro_app/ui/widgets/edit_workout/muscle_targets.dart';
 import 'package:vitaro_app/ui/widgets/edit_workout/workout_exercise_item.dart';
 
@@ -12,49 +12,59 @@ class EditWorkoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.surfaceContainerLow,
-        title: Text(
-          'Editar',
-          style: Theme.of(context).textTheme.titleSmall,
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
-        ],
-      ),
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              delegate: EditWorkoutHeader(workout: workout),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              'Editar',
+              style: Theme.of(context).textTheme.titleSmall,
             ),
-            SliverPersistentHeader(
-              delegate: MuscleTargets(exercises: workout.exercises),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) =>
-                    WorkoutExerciseItem(exercise: workout.exercises[index]),
-                childCount: workout.exercises.length,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: TextButton.icon(
-                  onPressed: () {},
-                  label: Text('Adicionar Exercício'),
-                  icon: Icon(Icons.add),
+            pinned: true,
+            expandedHeight: 300,
+            backgroundColor: Theme.of(
+              context,
+            ).colorScheme.surfaceContainerLow,
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Theme.of(
+                  context,
+                ).colorScheme.surfaceContainerLow,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 128),
+                  child: EditWorkoutForm(workout: workout),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SliverPersistentHeader(
+            delegate: MuscleTargets(exercises: workout.exercises),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) =>
+                  WorkoutExerciseItem(exercise: workout.exercises[index]),
+              childCount: workout.exercises.length,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 64),
+              child: TextButton.icon(
+                onPressed: () {},
+                label: const Text('Adicionar Exercício'),
+                icon: const Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: ElevatedButton(
         style: ElevatedButton.styleFrom(minimumSize: const Size(72, 44)),
         onPressed: () {},
-        child: Icon(
+        child: const Icon(
           Icons.check,
           size: 24,
         ),
